1dfa56306ab3b68691d5bf429ff142942f1a78e4
Piotr Dziedzic <piotr.dziedzic1729@outlook.com>
2020-09-05 16:59:23 +0200

    Second commit

---
+++ src/main/java/com/kodilla/KodillaCalculatorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCalculatorProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCalculatorProjectApplication.class, args);
+    }
+
+}
